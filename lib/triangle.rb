9adf3fac9a1012ca6c52e411d35117e05e3150fa
Joey Cho <cjy12182000@naver.com>
2018-06-08 15:33:18 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,32 @@
+require 'pry'
 class Triangle
   # write code here
+  def initialize(one,two,three)
+    @sides = []
+    @sides << one,two,three
+  end
+
+  def sides
+    @sides
+  end
+
+  def kind
+    
+    self.sides.each do
+      |side| if side < 0
+        raise TriangleError
+      end
+
+      self.sides.combination(2).to_a.map do
+        |a,b| a+b
+      end
+      end  
+    end
+
+    
+        
+  end
+
+  class TriangleError < StandardError
+  end
 end
